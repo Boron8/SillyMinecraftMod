9274377fc04f84159132c89349b8011ced3d091e
Boron8 <emil.sv.17@gmail.com>
2026-06-09 18:51:54 +0200

    Improved /dev command

--- code/src/main/java/me/creeper/creepermodtest/commands/CommandDev.java
+++ code/src/main/java/me/creeper/creepermodtest/commands/CommandDev.java
@@ -1,5 +1,6 @@
 package me.creeper.creepermodtest.commands;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import me.creeper.creepermodtest.ExampleMod;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
@@ -8,9 +9,11 @@ import net.minecraft.item.Item;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.common.MinecraftForge;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 public class CommandDev extends CommandBase  {
     @Override
@@ -21,25 +24,45 @@ public class CommandDev extends CommandBase  {
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) {
-        List<IRecipe> recipes = CraftingManager.getInstance().getRecipeList();
+        if (args.length <= 0) return;
+        String arg = args[0];
+        if (Objects.equals(arg, "remove_recipe")) {
+            List<IRecipe> recipes = CraftingManager.getInstance().getRecipeList();
 
-        Iterator<IRecipe> iterator = recipes.iterator();
+            Iterator<IRecipe> iterator = recipes.iterator();
 
-        while (iterator.hasNext()) {
-            IRecipe recipe = iterator.next();
+            while (iterator.hasNext()) {
+                IRecipe recipe = iterator.next();
 
-            if (recipe.getRecipeOutput() != null && recipe.getRecipeOutput().getItem() == Items.bread) {
-                String id = Item.itemRegistry.getNameForObject(recipe.getRecipeOutput().getItem());
+                if (recipe.getRecipeOutput() != null && recipe.getRecipeOutput().getItem() == Items.bread) {
+                    String id = Item.itemRegistry.getNameForObject(recipe.getRecipeOutput().getItem());
 
-                sender.addChatMessage(new ChatComponentText("Removed recipe for '" + id + "'"));
+                    sender.addChatMessage(new ChatComponentText("Removed recipe for '" + id + "'"));
 
-                iterator.remove();
+                    iterator.remove();
 
-                return; // early stop, for only 1 item
+                    return; // early stop, for only 1 item
+                }
             }
+        } else if (Objects.equals(arg, "forge_init")) {
+            MinecraftForge.initialize();
+        } else if (Objects.equals(arg, "reg_item")) {
+            Item i = new DevItem();
+            GameRegistry.registerItem(i, i.getUnlocalizedName().substring(5));
         }
     }
 
     @Override
     public boolean canCommandSenderUseCommand(ICommandSender sender) { return ExampleMod.getMainConfig().dev_command; }
+
+
+    public static class DevItem extends Item {
+        public DevItem() {
+            this.setCreativeTab(ExampleMod.tabCreepermodtest);
+            this.setMaxStackSize(64);
+
+            this.setUnlocalizedName("itemDevItem");
+            this.setTextureName("minecraft:stone");
+        }
+    }
 }
