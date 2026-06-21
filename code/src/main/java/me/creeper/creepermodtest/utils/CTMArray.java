38bc61a31fc892fed2d48069a9dcb614a2852b62
Boron8 <emil.sv.17@gmail.com>
2026-06-21 14:12:16 +0200

    Made CMTArray hashable

--- code/src/main/java/me/creeper/creepermodtest/utils/CTMArray.java
+++ code/src/main/java/me/creeper/creepermodtest/utils/CTMArray.java
@@ -2,6 +2,7 @@ package me.creeper.creepermodtest.utils;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Objects;
 
 public class CTMArray<T> {
     private final Deque<T> deque = new ArrayDeque<>();
@@ -24,4 +25,22 @@ public class CTMArray<T> {
 
     @Override
     public String toString() { return deque.toString(); }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CTMArray<?>)) return false;
+        CTMArray<?> other = (CTMArray<?>)o;
+        return maxSize == other.maxSize
+                && deque.equals(other.deque);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(maxSize);
+        for (T element : deque) {
+            result = 31 * result + Objects.hashCode(element);
+        }
+        return result;
+    }
 }
