c1c0b0a9920f3cc495d923790516ddf484159999
Boron8 <emil.sv.17@gmail.com>
2026-08-18 12:35:29 +0200

    Added more tutorial pages

--- code/src/main/java/me/creeper/creepermodtest/commands/tutorialPages/CommandTutorial.java
+++ code/src/main/java/me/creeper/creepermodtest/commands/tutorialPages/CommandTutorial.java
@@ -14,12 +14,13 @@ public class CommandTutorial implements ICommand {
     static {
         TutorialPage blocksPage = new TutorialPage("Blocks");
         TutorialPage commandsPage = new TutorialPage("Commands");
+        TutorialPage otherModsPage = new TutorialPage("OtherMods");
 
         blocksPage.addSubPage("mallirus", new TutorialPage(
                 "Mallirus Ore",
                 Arrays.asList(
                         "Material - Rock",
-                        "Hardness - 2.5F",
+                        "Hardness - 2.5",
                         "Harvest - Pickaxe 2",
                         "Tab - tabCreepermodtest",
                         "Generation:",
@@ -27,7 +28,9 @@ public class CommandTutorial implements ICommand {
                         "  MaxVeinSize: 10",
                         "  MinY: 0",
                         "  MaxY: 70",
-                        "  Replaces: Stone"
+                        "  Replaces: Stone",
+                        "  Dimensions: Overworld",
+                        "Descriptions: A common ore (i will make it less common), used to make other crafting materials"
                 )
         ));
         blocksPage.addSubPage("table", new TutorialPage(
@@ -35,10 +38,45 @@ public class CommandTutorial implements ICommand {
                 Arrays.asList(
                         "Table Block:",
                         "Material - Wood",
-                        "Hardness - 2.0F",
+                        "Hardness - 2.0",
                         "Harvest - Axe 1",
                         "Light Level - 0.1F",
-                        "Tab - tabCreepermodtest"
+                        "Tab - tabCreepermodtest",
+                        "Descriptions: An early test block"
+                )
+        ));
+        blocksPage.addSubPage("copper", new TutorialPage(
+                "Copper Ore",
+                Arrays.asList(
+                        "Material - Rock",
+                        "Hardness - 2.5",
+                        "Harvest - Pickaxe 2",
+                        "Tab - tabCreepermodtest",
+                        "Generation:",
+                        "  MinVeinSize: 3",
+                        "  MaxVeinSize: 8",
+                        "  MinY: 15",
+                        "  MaxY: 60",
+                        "  Replaces: Stone",
+                        "  Dimensions: Overworld",
+                        "Descriptions: A common ore "
+                )
+        ));
+        blocksPage.addSubPage("tungsten", new TutorialPage(
+                "Tungsten Ore",
+                Arrays.asList(
+                        "Material - Rock",
+                        "Hardness - 4",
+                        "Harvest - Pickaxe 3",
+                        "Tab - tabCreepermodtest",
+                        "Generation:",
+                        "  MinVeinSize: 2",
+                        "  MaxVeinSize: 6",
+                        "  MinY: 10",
+                        "  MaxY: 38",
+                        "  Replaces: Netherrack",
+                        "  Dimensions: Nether",
+                        "Descriptions: A rare-ish ore found in nether, originally made for the End but that didn't work, maybe ill do something in the end later"
                 )
         ));
 
@@ -49,7 +87,6 @@ public class CommandTutorial implements ICommand {
                         "Permission - All, 0",
                         "Usage - /version",
                         "Description - Shows current mod version and info."
-
                 )
         ));
         commandsPage.addSubPage("tutorial", new TutorialPage(
@@ -70,9 +107,45 @@ public class CommandTutorial implements ICommand {
                         "Description - Damages a player with 1.0F of outOfWorld damage."
                 )
         ));
+        commandsPage.addSubPage("licenses", new TutorialPage(
+                "Licenses",
+                Arrays.asList(
+                        "Side - Client",
+                        "Permission - All, 0",
+                        "Usage - /licenses [project]",
+                        "Description - Displays a license for different projects, no project shows what projects are available."
+                )
+        ));
+        commandsPage.addSubPage("tps", new TutorialPage(
+                "Tps",
+                Arrays.asList(
+                        "Side - Server",
+                        "Permission - All, 0",
+                        "Usage - /tps",
+                        "Description - Shows MSPT (milli-seconds per tick). Displays MSPT over 10/20/50/100 ticks. One tick may last max 50ms before TPS lag. If MSPT is lower than 50ms, will cause TPS lag. Lower MSPT is better, 0=ideal 0-50=ok 50=max 50+=bad, Actual TPS display may come later."
+                )
+        ));
+
+        otherModsPage.addSubPage("required", new TutorialPage(
+                "Required Mods",
+                Arrays.asList(
+                        "Any Mixin provider (UniMixins Recommended) - Provides the mixin library to minecraft"
+                )
+        ));
+        otherModsPage.addSubPage("recommended", new TutorialPage(
+                "Recommended Mods",
+                Arrays.asList(
+                        "Laggoggles backported - See whats lagging your world",
+                        "(GTNH) NotEnoughItems - See items recipes easily, GTNH version provides extra functionality like bookmarks and GUI tabs",
+                        "Waila - What Am I Looking At, see what your pointing at",
+                        "Spark - See whats lagging, primarily for servers, lag goggles is recommended over spark",
+                        "JourneyMap / MapWriter 2 - A minimap and world map to see your surroundings and what you've explored, MapWriter is a good opensource alternative to JourneyMap"
+                )
+        ));
 
         pages.put("blocks",    blocksPage);
-        pages.put("commands", commandsPage);
+        pages.put("commands",  commandsPage);
+        pages.put("othermods", otherModsPage);
     }
 
 
